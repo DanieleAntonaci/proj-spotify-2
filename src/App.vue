169e91db860bd662a3c6484d0290031f067b113e
DanieleAntonaci <danieleanto96@gmail.com>
2022-12-26 11:07:28 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import Player from './components/Player.vue';
+export default {
+  data() {
+    return {
+
+    }
+  }, components: {
+    HelloWorld,
+    Player
+  }
+}
+</script>
+
+<template>
+  <div>
+    <HelloWorld/>
+    <Player/>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Player.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <div id="player">
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#player {
+    background-color: #181818;
+}
+</style>
